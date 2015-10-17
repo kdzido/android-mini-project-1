69b98431e0f5305787e39a3ac1db1708c2525cd8
edzikrz <krzysztof.dzido@ericsson.com>
2015-10-17 20:47:46 +0200

    what: - playing with opening new activities with intents - playing with shared preferences

--- app/src/main/java/pl/edu/pja/s13868/miniproject1/MyMainActivity.java
+++ app/src/main/java/pl/edu/pja/s13868/miniproject1/MyMainActivity.java
@@ -1,15 +1,23 @@
 package pl.edu.pja.s13868.miniproject1;
 
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Button;
 
 public class MyMainActivity extends AppCompatActivity {
+
+    private static final String MY_PREFS_NAME = "mini-project-1-prefs" ;
+    public static final String PRODUCT_1_KEY = "product_1";
+    public static final String PROLDUCT_1_VALUE = "value_1";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,6 +34,34 @@ public class MyMainActivity extends AppCompatActivity {
                         .setAction("Action", null).show();
             }
         });
+
+        // List button
+        Button listButton = (Button) findViewById(R.id.listButton);
+        listButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(MyMainActivity.this, MyListActivity.class));
+            }
+        });
+
+        // Options button
+        Button optionsButton = (Button) findViewById(R.id.optionsButton);
+        optionsButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(MyMainActivity.this, MyOptionsActivity.class));
+            }
+        });
+
+        SharedPreferences.Editor editor = getSharedPreferences(MY_PREFS_NAME, MODE_PRIVATE).edit();
+        editor.putString(PRODUCT_1_KEY, PROLDUCT_1_VALUE);
+        editor.commit();
+        Log.i("tag", "Stored shared pref '" + PRODUCT_1_KEY + "':" + PROLDUCT_1_VALUE);
+
+        SharedPreferences settings = getSharedPreferences(MY_PREFS_NAME, MODE_PRIVATE);
+        String value = settings.getString(PRODUCT_1_KEY, null);
+        Log.i("tag", "Loaded shared pref '" + PRODUCT_1_KEY + "':" +  value);
+
 
 
     }
