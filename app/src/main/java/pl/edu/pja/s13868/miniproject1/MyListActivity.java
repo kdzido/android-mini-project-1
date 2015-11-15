c2183c4332d81b8c173f41dfa896509d35dd4541
edzikrz <krzysztof.dzido@ericsson.com>
2015-11-15 12:39:57 +0100

    clean

--- app/src/main/java/pl/edu/pja/s13868/miniproject1/MyListActivity.java
+++ app/src/main/java/pl/edu/pja/s13868/miniproject1/MyListActivity.java
@@ -2,15 +2,17 @@ package pl.edu.pja.s13868.miniproject1;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ListView;
 import android.widget.Toast;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import pl.edu.pja.s13868.miniproject1.domain.model.product.Product;
 import pl.edu.pja.s13868.miniproject1.domain.model.product.ProductRepository;
-import pl.edu.pja.s13868.miniproject1.ui.list.ProductAdapter;
+import pl.edu.pja.s13868.miniproject1.ui.list.ProductArrayAdapter;
 
 /**
  * @author Krzysztof Dzido <s13868@pjwstka.edu.pl>
@@ -18,7 +20,7 @@ import pl.edu.pja.s13868.miniproject1.ui.list.ProductAdapter;
 public class MyListActivity extends AppCompatActivity {
 
     private ProductRepository productRepository;
-    private ProductAdapter productAdapter;
+    private ProductArrayAdapter productAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,28 +29,33 @@ public class MyListActivity extends AppCompatActivity {
 
         this.productRepository = SingletonRegistry.INSTANCE.productRepositorySingleton();
 
-        Product[] productItems = this.productRepository.listAllProductsAsArray();
-        this.productAdapter = new ProductAdapter(this, productItems);
+        List<Product> productItems = new ArrayList<>(this.productRepository.listAllProducts());
+        this.productAdapter = new ProductArrayAdapter(this, productItems);
 
         ListView productListView = (ListView) findViewById(R.id.listView);
-        productListView.setChoiceMode(productListView.CHOICE_MODE_MULTIPLE);
+//        productListView.setChoiceMode(productListView.CHOICE_MODE_MULTIPLE);
+
         productListView.setAdapter(this.productAdapter);
 
         productListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
             public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-                Product product = MyListActivity.this.productAdapter.getItem(position);
+                Toast.makeText(getApplicationContext(), "Item " + position, Toast.LENGTH_SHORT).show();
 
-
-                Log.i("product_list_before", product.toString());
-                if (product.isBought()) {
-                    product.markAsNotBought();
-                } else {
-                    product.markAsBought();
-                }
-                Log.i("product_list_after", product.toString());
+//                Product product = MyListActivity.this.productAdapter.getItem(position);
+//
+//
+//                Log.i("product_list_before", product.toString());
+//                if (product.isBought()) {
+//                    product.markAsNotBought();
+//                } else {
+//                    product.markAsBought();
+//                }
+//                Log.i("product_list_after", product.toString());
             }
         });
+
+
     }
 
 }
